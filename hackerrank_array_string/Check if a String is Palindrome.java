c297c1d171781cdd6152fb6e43bf7e50beb64c49
Bharathkh-04 <bharathkh04@gmail.com>
2025-09-01 21:01:28 +0530

    Check if a String is Palindrome

---
+++ hackerrank_array_string/Check if a String is Palindrome.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int n=s.length();
+        String rev="";
+        for(int i=n-1;i>=0;i--)
+        {
+            rev+=s.charAt(i);
+        }
+        if(s.equals(rev))
+        {
+            System.out.print("YES");
+        }
+        else
+            System.out.print("NO");
+    }
+}
