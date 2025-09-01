b30cf5c49659ed080b148dbcbfcde64b453fc001
Bharathkh-04 <bharathkh04@gmail.com>
2025-09-01 21:09:23 +0530

    Count the Number of Words in a Sentence

---
+++ hackerrank_array_string/Count the Number of Words in a Sentence.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int n=s.length();
+        String arr[]=s.trim().split("\\s+");
+        System.out.print(arr.length);
+    }
+}
